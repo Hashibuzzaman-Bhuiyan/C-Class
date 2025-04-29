4d122ae626222e4e3307281d1e5d96c411f23333
HASHIB <hashibuzzamanbhuiyan7@gmail.com>
2025-04-29 14:23:03 +0600

    Delete Array_Statement directory

--- Array_Statement/Name_output/Name_output.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    char s[80];
-    printf("Enter a Name");
-    scanf("%s",s);
-    printf("Hello %s",s);
-}
--- Array_Statement/Text_counter_between_string/Text_counter_between_string.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    char s[80];
-    int l;
-    printf("Enter a String");
-    scanf("%s,s");
-    printf("Number of Character is %d",strlen(s));
-}
